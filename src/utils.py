18447933e1a57ee25b51e814afe5c335e3a22933
Spartan <dronajagad44@gmail.com>
2024-03-13 13:33:34 -0700

    Hyperparameter Training for different Models

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,9 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
+
 
 def save_object(file_path, obj):
     try:
@@ -17,18 +19,23 @@ def save_object(file_path, obj):
             dill.dump(obj, file_obj)
 
     except Exception as e:
-        raise Custo
+        raise CustomException(e, sys)
     
-def evaluate_model(X_train, X_test, y_train, y_test, models):
+def evaluate_model(X_train, y_train,X_test,y_test,models,param):
     try:
-
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
-            #Train Model
-            model.fit(X_train, y_train) 
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
 
@@ -41,6 +48,6 @@ def evaluate_model(X_train, X_test, y_train, y_test, models):
             report[list(models.keys())[i]] = test_model_score
 
         return report
-    
+
     except Exception as e:
         raise CustomException(e, sys)
